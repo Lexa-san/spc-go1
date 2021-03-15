6e70784ba341b533a0cd04f333b68d1daadb155e
Lexa-san <ryoha.san@gmail.com>
2021-03-15 10:30:30 +0300

    finish Final Task

--- taskFinal/main.go
+++ taskFinal/main.go
@@ -7,6 +7,7 @@ import (
 	"sort"
 	"strconv"
 	"strings"
+	"unicode"
 )
 
 var taskTraker = make(map[string]map[string]string)
@@ -31,6 +32,7 @@ taskTrakerActionLoop:
 			actionStartApp()
 		case "Quit":
 			break taskTrakerActionLoop
+			//actionQuit()
 		case "Print":
 			if !isWorking {
 				continue
@@ -45,10 +47,12 @@ taskTrakerActionLoop:
 			if !isWorking {
 				continue
 			}
+			actionDel(context)
 		case "Find":
 			if !isWorking {
 				continue
 			}
+			actionFind(context)
 		}
 	}
 
@@ -57,7 +61,12 @@ taskTrakerActionLoop:
 // get action name as string and additional data of action in slice
 func listenAction(scanner *bufio.Scanner) (action string, context []string) {
 	scanner.Scan()
-	arr := strings.Split(scanner.Text(), " ")
+	str := scanner.Text()
+	// remove spaces from right side of scanned string
+	str = strings.TrimRightFunc(str, func(r rune) bool {
+		return !unicode.IsLetter(r) && !unicode.IsNumber(r)
+	})
+	arr := strings.Split(str, " ")
 
 	if len(arr) == 1 {
 		return arr[0], nil
@@ -87,6 +96,12 @@ func actionStartApp() {
 	isWorking = true
 }
 
+// Action Quit
+//func actionQuit() {
+//	isWorking = false
+//}
+
+// Action Add
 func actionAdd(context []string) {
 	var date, task string = context[0], context[1]
 	date = normalizeData(date)
@@ -97,6 +112,7 @@ func actionAdd(context []string) {
 	taskTraker[date][task] = task
 }
 
+// Action Print
 func actionPrint() {
 	// sort dates
 	dates := make([]string, 0, len(taskTraker))
@@ -105,20 +121,70 @@ func actionPrint() {
 	}
 	sort.Strings(dates)
 
-	var tasks []string
+	//var tasks []string
 	for _, date := range dates {
 
-		// sort tasks of every date
-		tasks = make([]string, 0, len(taskTraker[date]))
-		for task := range taskTraker[date] {
-			tasks = append(tasks, task)
-		}
-		sort.Strings(tasks)
-
-		// final print
-		for _, task := range tasks {
-			fmt.Println(date, task)
-		}
+		printOneDateTasks(date, true)
 
 	}
+}
+
+func printOneDateTasks(date string, withDate bool) {
+	// sort tasks
+	tasks := make([]string, 0, len(taskTraker[date]))
+	for task := range taskTraker[date] {
+		tasks = append(tasks, task)
+	}
+	sort.Strings(tasks)
+
+	// final print
+	for _, task := range tasks {
+		if withDate {
+			fmt.Printf("%s ", date)
+		}
+		fmt.Printf("%s\n", task)
+	}
+}
+
+// Action Find
+func actionFind(context []string) {
+	var date string = context[0]
+	date = normalizeData(date)
+
+	if _, ok := taskTraker[date]; ok {
+		printOneDateTasks(date, false)
+	}
+}
+
+// Action Del
+func actionDel(context []string) {
+	var date string = context[0]
+	date = normalizeData(date)
+
+	if len(context) == 1 {
+		delTasksByDate(date)
+	}
+	if len(context) == 2 {
+		delOneTask(date, context[1])
+	}
+}
+
+func delTasksByDate(date string) {
+	if tasks, ok := taskTraker[date]; ok {
+		delete(taskTraker, date)
+		fmt.Printf("Deleted %d events\n", len(tasks))
+		return
+	}
+
+	fmt.Println("Deleted 0 events")
+}
+
+func delOneTask(date string, task string) {
+	if _, ok := taskTraker[date][task]; ok {
+		delete(taskTraker[date], task)
+		fmt.Println("Deleted successfully")
+		return
+	}
+
+	fmt.Println("Event not found")
 }
